88e4d8eac526bcf463ec3ac85f97b46ff44078a3
Magda Lejwoda <le.magda@o2.pl>
2019-09-16 20:15:43 +0200

    Change method app.initMenu

--- src/js/script.js
+++ src/js/script.js
@@ -54,8 +54,13 @@
 
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      const thisApp = this;
+
+      console.log('thisApp.data', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
 
     initData: function(){
