f4df23cd475942c4b395ab4785e823c415e9f250
Magda Lejwoda <le.magda@o2.pl>
2019-09-25 22:57:35 +0200

    Add initAction to show an order basket

--- src/js/script.js
+++ src/js/script.js
@@ -308,8 +308,8 @@
       const thisCart = this;
 
       thisCart.products = [];
-
       thisCart.getElements(element);
+      thisCart.initActions();
 
       console.log('new Card', thisCart);
     }
@@ -320,9 +320,18 @@
       thisCart.dom = {};
 
       thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+      thisCart.dom.wrapper.classList.toggle('active');
+      });
     }
   }
-
   const app = {
 
     initMenu: function(){
